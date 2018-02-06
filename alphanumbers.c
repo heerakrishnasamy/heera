9f9de40abceb58b67b50b559334ac9c7567d3e7e
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-02-06 20:03:01 +0530

    Create alphanumbers.c

---
+++ alphanumbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char s[100];
+  int i,c=0,c1=0;
+  scanf("%[^\n]s",&s);
+  for(i=0;s[i]!='\0';i++)
+  {
+      
+    if( (s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
+    {
+        c=c+1;
+    }
+   else if(s[i]>=0||s[i]<=9)
+    {
+        c1=c1+1;
+    }
+  }
+  if(c!=0&&c1!=0)
+  {
+      printf("\n String contains both alphabet and number");
+  }
+  else
+  {
+      printf("no");
+  }
+  
+}
