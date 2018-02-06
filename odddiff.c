5c024650813772f736361bbdce70924e16b3b09e
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-02-06 21:05:51 +0530

    Create odddiff.c

---
+++ odddiff.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int s,t,r;
+    scanf("%d%d",&s,&t);
+    r=s-t;
+    if(r%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
+    
