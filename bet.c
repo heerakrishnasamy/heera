195c748b6d8edaeec8ad66418644ee3189021c32
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-02-09 14:57:52 +0530

    Create bet.c

---
+++ bet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int n1,n2,a;
+ scanf("%d",&a);
+ scanf("%d%d",&n1,&n2);
+ if(a>=n1&&a<=n2)
+ {
+     printf("yes");
+ }
+ else
+ {
+     printf("no");
+ }
+ return 0;
+}
