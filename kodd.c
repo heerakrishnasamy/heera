d87203f5e29f26787da30109decadbecb3569c46
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-03-01 16:36:23 +0530

    Create kodd.c

---
+++ kodd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,k,i,flag=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    if((a[i]%2)!=0)
+    {
+        flag++;
+    
+    }
+    if(flag==k)
+    {
+        printf("%d",a[i]);
+        break;
+    }
+    }
+    return 0;
+}
