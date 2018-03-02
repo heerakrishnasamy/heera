9060742fd6452edc8c604961fd5156ad6147e381
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-03-02 14:51:58 +0530

    Create firstcap.c

---
+++ firstcap.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10],b[10];
+int i,j,l;
+scanf("%[^\n]a",&a);
+l=strlen(a);
+a[0]=a[0]-32;
+printf("%c ",a[0]);  
+for(i=1;i<=l;i++)
+{
+if(a[i]==' ')
+{
+b[i]=a[i+1]-32;
+printf("%c",b[i]);
+}
+else
+{
+printf("%c",a[i+1]);
+}
+}
+return 0;
+}
