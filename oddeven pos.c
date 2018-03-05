46b9a91762b319dbc633ddfe162dc29961389b25
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-03-05 22:16:20 +0530

    Create oddeven pos.c

---
+++ oddeven pos.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main()
+{
+   char a[20];
+   int i;
+   scanf("%s",&a);
+   for(i=0;a[i]!='\0';i++)  
+   {
+       printf("%c",a[i]);
+       i++;
+   }
+   printf(" ");
+   for(i=1;a[i]!='\0';i++)
+   {
+       printf("%c",a[i]);
+       i++;
+   }
+   return 0;
+}
+  
+
